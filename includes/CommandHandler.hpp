efdf76ccbb3a35bf30af5b1b44b86f52105ac785
Veronique Galmiche <vgalmich@f4r4s14.clusters.42paris.fr>
2025-12-19 16:57:06 +0100

    files revised

--- includes/CommandHandler.hpp
+++ includes/CommandHandler.hpp
@@ -1,33 +1,22 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   CommandHandler.hpp                                 :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: vgalmich <vgalmich@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/06 14:09:59 by vgalmich          #+#    #+#             */
-/*   Updated: 2025/12/19 16:39:03 by vgalmich         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#ifndef COMMANDHANDLER_HPP
+#define COMMANDHANDLER_HPP
 
-#pragma once
-
-#include "Channel.hpp"
 #include "Server.hpp"
+#include "Channel.hpp"
 #include "Client.hpp"
 #include "NumericReplies.hpp"
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <map>
-#include <sstream>
 
 class CommandHandler
 {
     public:
         CommandHandler(Server *server);
         
-        // typedef pour pointeur vers fonctions membres de CommandHandler
+        // typedef for pointer to member functions of CommandHandler
         typedef void (CommandHandler::*CommandHandlerFunction)(Client* client, const std::vector<std::string>&);
         
         void processCommand(Client* client, const std::string &input);
@@ -35,42 +24,42 @@ class CommandHandler
     private:
         Server *_server;
         
-        // Map des commandes vers leurs handlers
+        // map of commands to their handlers
         std::map<std::string, CommandHandlerFunction> _commandMap;
         
-        // Initialiser la map
         void _initCommandMap();
         
-        // Parse une commande IRC brute en commande + params
         void _parseInput(const std::string &input, std::string &command, std::vector<std::string> &params);
         
-        // ========== AUTH COMMANDS ==========
+        // AUTH COMMANDS
         void cmdPass(Client* client, const std::vector<std::string> &params);
         void cmdNick(Client* client, const std::vector<std::string> &params);
         void cmdUser(Client* client, const std::vector<std::string> &params);
         
-        // ========== CHANNEL COMMANDS ==========
+        // CHANNEL COMMANDS 
         void cmdJoin(Client* client, const std::vector<std::string> &params);
         void cmdPart(Client* client, const std::vector<std::string> &params);
         void cmdNames(Client* client, const std::vector<std::string> &params);
         
-        // ========== MESSAGE COMMANDS ==========
+        // MESSAGE COMMANDS
         void cmdPrivmsg(Client* client, const std::vector<std::string> &params);
         void cmdNotice(Client* client, const std::vector<std::string> &params);
         
-        // ========== OPERATOR COMMANDS ==========
+        // OPERATOR COMMANDS
         void cmdKick(Client* client, const std::vector<std::string> &params);
         void cmdInvite(Client* client, const std::vector<std::string> &params);
         void cmdTopic(Client* client, const std::vector<std::string> &params);
         void cmdMode(Client* client, const std::vector<std::string> &params);
         
-        // Commandes utilitaires
+        // UTILITY COMMANDS
         void cmdQuit(Client* client, const std::vector<std::string> &params);
         void cmdPing(Client* client, const std::vector<std::string> &params);
         
-        // Helpers
+        // HELPERS
         void sendWelcomeMsg(Client* client);
         void sendNumericReply(Client* client, const std::string& numeric, const std::string& message);
         bool isValidNickname(const std::string& nickname);
         bool isValidChannelName(const std::string& name);
 };
+
+#endif
