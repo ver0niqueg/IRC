40bf51b93e2d3fe583f11f1c6463b6f6a33476f4
Clio Dalmaz <cldalmaz@f4r11s4.clusters.42paris.fr>
2026-01-21 16:13:22 +0100

    added blue

--- includes/Colors.hpp
+++ includes/Colors.hpp
@@ -5,6 +5,7 @@
 #define PASTEL_VIOLET "\033[1;38;5;183m"
 #define PASTEL_GREEN  "\033[1;38;5;120m"
 #define PASTEL_RED    "\033[1;38;5;203m"
+#define PASTEL_BLUE   "\033[1;38;5;75m"
 #define PASTEL_YELLOW "\033[1;38;5;228m"
 
 #endif
