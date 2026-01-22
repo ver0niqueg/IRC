84f80166ec60d86f4f86012c772f4ac98774e164
Veronique Galmiche <vgalmich@f4r1s14.clusters.42paris.fr>
2026-01-22 16:50:55 +0100

    colors description added

--- includes/Colors.hpp
+++ includes/Colors.hpp
@@ -1,13 +1,13 @@
 #ifndef COLORS_HPP
 #define COLORS_HPP
 
-#define DEFAULT         "\033[0m"
+#define DEFAULT       "\033[0m"          // Reset color to terminal default
 
-#define PASTEL_VIOLET   "\033[1;38;5;183m" // [INFO]
-#define PASTEL_GREEN    "\033[1;38;5;120m" // [SEND]
-#define PASTEL_RED      "\033[1;38;5;203m" // [Sever stopped]
-#define PASTEL_BLUE     "\033[1;38;5;75m" // [RECV]
-#define PASTEL_YELLOW   "\033[1;38;5;228m" // [CONNECTION/DISCONNECTION]
-#define PASTEL_ORANGE   "\033[1;38;5;215m"  // [WARN]
+#define PASTEL_VIOLET "\033[1;38;5;183m" // General information (server state, setup)
+#define PASTEL_GREEN  "\033[1;38;5;120m" // Send messages (successful operations, confirmations)
+#define PASTEL_RED    "\033[1;38;5;203m" // Info when server stops or errors (invalid commands, failures)
+#define PASTEL_BLUE   "\033[1;38;5;75m"  // Receive messages (incoming data, client actions)
+#define PASTEL_YELLOW "\033[1;38;5;228m" // Warnings (non-fatal issues, edge cases)
+#define PASTEL_ORANGE "\033[1;38;5;215m" // Important notices (disconnects, critical events)
 
 #endif
